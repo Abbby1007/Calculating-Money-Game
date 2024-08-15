248e29aa1737b2ec276371ce84a0fee09e17fb3c
Abbby1007 <Abigailloken25@gmail.com>
2024-08-15 07:22:18 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const pencil
+function level1Q1(){
+if(pencil == 1){
+  document.getElementById("display").innerHTML=`Hello`
+}
+  else {
+    document.getElementById("display").innerHTML=`:(`
+  }
+  
+}
