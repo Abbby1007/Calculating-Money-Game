b87bfa664450161f7fcb10a788d613902d2d2449
Abbby1007 <Abigailloken25@gmail.com>
2024-08-24 11:14:37 +0000

    Complete Level 6 (Medium)

--- scriptMedLevel6.js
+++ scriptMedLevel6.js
@@ -269,3 +269,36 @@ function medLevel6Q9(){
 }
 
 // Medium  DAY 6 (LEVEL 6)  CUSTOMER 9 (QUESTION 9) End
+
+// Medium DAY 6 (LEVEL 6)  CUSTOMER 10 (QUESTION 10) START
+function medLevel6Q10(){
+ // Names
+  const name = ["Stanley", "Trevor", "Eva", "Poppy","Darren","Jake","Selena","Ariana","Leonard","Troy","Makenzie","Logan","Emma","Abigail","Keana","Isabella","Skyla","Zachary","Jude","Hailey","Emily","Kayla","Jessica","Carol","Alex","Noah","Sadie","Michael","Julia","Sarah","Ashley","Madison","Howard","Sheryl","Adriyel"]
+   const randomName = name[Math.floor(Math.random() * name.length)];
+
+  //Compliments
+   const compliments = ["Wow You're a SUPERSTAR ⭐!!!!", "Your Awesome ❤️!!!!","Thank you Math Wiz 🔢!!!","Thank you ❤️!!!!", " I appreaciate this❤️!!!!❤️"];
+   const randomcompliment = compliments[Math.floor(Math.random() * compliments.length)];
+  document.getElementById("questionTitle").innerHTML= `Customer 10 (Question 10)`
+
+  document.getElementById("question").innerHTML= `<b> ${randomName} </b> I have 20 pennies, 15 nickels, 23 dimes, 31 quarters, and 25 fifty-cents... . How much money do I have? ` 
+
+  var input = document.getElementById("answerInput").value;
+
+    console.log(input)
+    var correct = 23.50 ;
+
+  if(input == correct){
+     document.getElementById("display").innerHTML=` <b> ${randomName}:</b> ${randomcompliment} <br><button onclick="medLevel6End()"> Finish Day 6 </button>`
+
+  }
+    else {
+      document.getElementById("display").innerHTML=`Try Again`
+    }
+}
+
+// MED  DAY 6 (LEVEL 6)  CUSTOMER 10 (QUESTION 10) END
+
+function medLevel6End(){
+  document.getElementById("display").innerHTML=` <b> Boss :</b> Congratulations for Completing Day 6 <br> <a href="MedLeveleeeeeee.html"><button> Move on to Medium Day 7 (Level 7) </button> </a>`
+}
