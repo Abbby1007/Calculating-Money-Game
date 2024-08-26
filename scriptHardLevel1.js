752fc0b604d3ea32b372e9f9bb0d1236f15994b7
Abbby1007 <Abigailloken25@gmail.com>
2024-08-26 01:59:30 +0000

    Adding Questions for HArd mode level 1

--- scriptHardLevel1.js
+++ scriptHardLevel1.js
@@ -17,3 +17,35 @@ if(input == correct){
 
 }
 // Hard  DAY 1 (LEVEL 1)  CUSTOMER 1 (QUESTION 1) END
+
+// Hard DAY 1 (LEVEL 1)  CUSTOMER 2 (QUESTION 2) START
+
+function hardLevel1Q2(){
+  // Names
+  const name = ["Stanley", "Trevor", "Eva", "Poppy","Darren","Jake","Selena","Ariana","Leonard","Troy","Makenzie","Logan","Emma","Abigail","Keana","Isabella","Skyla","Zachary","Jude","Hailey","Emily","Kayla","Jessica","Carol","Alex","Noah","Sadie","Michael","Julia","Sarah","Ashley","Madison","Howard","Sheryl","Adriyel"]
+   const randomName = name[Math.floor(Math.random() * name.length)];
+
+  //Compliments
+   const compliments = ["Wow You're a SUPERSTAR ⭐!!!!", "Your Awesome ❤️!!!!","Thank you Math Wiz 🔢!!!","Thank you ❤️!!!!", " I appreaciate this❤️!!!!❤️"];
+   const randomcompliment = compliments[Math.floor(Math.random() * compliments.length)];
+  document.getElementById("questionTitle").innerHTML= `Customer 2 (Question 2)`
+
+  document.getElementById("question").innerHTML= `<b> ${randomName} </b> I have 18 pennies, 25 nickels, 35 dimes, 22 quarters, and 18 fifty-cents... . How much money do I have? ` 
+
+
+
+  var input = document.getElementById("answerInput").value;
+
+    console.log(input)
+    var correct = 19.43;
+
+  if(input == correct){
+     document.getElementById("display").innerHTML=` <b> ${randomName}:</b> ${randomcompliment} <br><button onclick="hardLevel1Q3()"> Customer 3 </button>`
+    document.getElementById("submitButton").innerHTML = `<button onclick="hardLevel1Q3()"> Submit</button>`
+
+  }
+    else {
+      document.getElementById("display").innerHTML=`Try Again`
+    }
+}
+// Hard  DAY 1 (LEVEL 1)  CUSTOMER 2 (QUESTION 2) END
