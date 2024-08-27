84d93514706000dd9d879eb091fc4a11e64e405f
Abbby1007 <Abigailloken25@gmail.com>
2024-08-27 20:12:32 +0900

    Fixing problems for Level 2, 3 and 4 for Hard mode

--- scriptHardLevel2.js
+++ scriptHardLevel2.js
@@ -275,7 +275,32 @@ function hardLevel2Q9(){
 // Hard  Day 2 (LEVEL 2)  CUSTOMER 9 (QUESTION 9) END
 
 
+// Hard DAY 2 (LEVEL 2)  CUSTOMER 10 (QUESTION 10) START
+function hardLevel2Q10(){
+ // Names
+  const name = ["Stanley", "Trevor", "Eva", "Poppy","Darren","Jake","Selena","Ariana","Leonard","Troy","Makenzie","Logan","Emma","Abigail","Keana","Isabella","Skyla","Zachary","Jude","Hailey","Emily","Kayla","Jessica","Carol","Alex","Noah","Sadie","Michael","Julia","Sarah","Ashley","Madison","Howard","Sheryl","Adriyel"]
+   const randomName = name[Math.floor(Math.random() * name.length)];
 
+  //Compliments
+   const compliments = ["Wow You're a SUPERSTAR ⭐!!!!", "Your Awesome ❤️!!!!","Thank you Math Wiz 🔢!!!","Thank you ❤️!!!!", " I appreaciate this❤️!!!!❤️"];
+   const randomcompliment = compliments[Math.floor(Math.random() * compliments.length)];
+  document.getElementById("questionTitle").innerHTML= `Customer 10 (Question 10)`
+
+  document.getElementById("question").innerHTML= `<b> ${randomName} </b> I have 16 pennies, 15 nickels, 22 dimes, 35 quarters, and 17 fifty-cents... . How much money do I have? ` 
+
+  var input = document.getElementById("answerInput").value;
+
+    console.log(input)
+    var correct = 20.36 ;
+
+  if(input == correct){
+     document.getElementById("display").innerHTML=` <b> ${randomName}:</b> ${randomcompliment} <br><button onclick="hardLevel2End()"> Finish Day 2 </button>`
+
+  }
+    else {
+      document.getElementById("display").innerHTML=`Try Again`
+    }
+}
 // Hard  DAY 2 (LEVEL 2)  CUSTOMER 10 (QUESTION 10) END
 
 function hardLevel2End(){
--- scriptHardLevel4.js
+++ scriptHardLevel4.js
@@ -273,3 +273,31 @@ function hardLevel4Q9(){
     }
 }
 // Hard  DAY 4 (LEVEL 4)  CUSTOMER 9 (QUESTION 9) END
+
+// Hard DAY 4 (LEVEL 4)  CUSTOMER 10 (QUESTION 10) START
+function hardLevel4Q10(){
+ // Names
+  const name = ["Stanley", "Trevor", "Eva", "Poppy","Darren","Jake","Selena","Ariana","Leonard","Troy","Makenzie","Logan","Emma","Abigail","Keana","Isabella","Skyla","Zachary","Jude","Hailey","Emily","Kayla","Jessica","Carol","Alex","Noah","Sadie","Michael","Julia","Sarah","Ashley","Madison","Howard","Sheryl","Adriyel"]
+   const randomName = name[Math.floor(Math.random() * name.length)];
+
+  //Compliments
+   const compliments = ["Wow You're a SUPERSTAR ⭐!!!!", "Your Awesome ❤️!!!!","Thank you Math Wiz 🔢!!!","Thank you ❤️!!!!", " I appreaciate this❤️!!!!❤️"];
+   const randomcompliment = compliments[Math.floor(Math.random() * compliments.length)];
+  document.getElementById("questionTitle").innerHTML= `Customer 10 (Question 10)`
+
+  document.getElementById("question").innerHTML= `<b> ${randomName} </b> I have 26 pennies, 39 nickels, 20 dimes, 25 quarters, and 21 fifty-cents... . How much money do I have? ` 
+
+  var input = document.getElementById("answerInput").value;
+
+    console.log(input)
+    var correct = 20.96 ;
+
+  if(input == correct){
+     document.getElementById("display").innerHTML=` <b> ${randomName}:</b> ${randomcompliment} <br><button onclick="hardLevel4End()"> Finish Day 4 </button>`
+
+  }
+    else {
+      document.getElementById("display").innerHTML=`Try Again`
+    }
+}
+// Hard  DAY 4 (LEVEL 4)  CUSTOMER 10 (QUESTION 10) END
