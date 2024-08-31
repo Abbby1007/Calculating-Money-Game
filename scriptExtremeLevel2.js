70952d045deb8b875947c777ce5f550c4086b1f9
Abbby1007 <Abigailloken25@gmail.com>
2024-08-31 09:24:34 +0900

    Finish Questions for Level 2 Extreme

--- scriptExtremeLevel2.js
+++ scriptExtremeLevel2.js
@@ -273,3 +273,35 @@ function extremeLevel2Q9(){
     }
 }
 // Extreme  DAY 2 (LEVEL 2)  CUSTOMER 9 (QUESTION 9) END
+
+// Extreme DAY 2 (LEVEL 2)  CUSTOMER 10 (QUESTION 10) START
+function extremeLevel2Q10(){
+ // Names
+  const name = ["Stanley", "Trevor", "Eva", "Poppy","Darren","Jake","Selena","Ariana","Leonard","Troy","Makenzie","Logan","Emma","Abigail","Keana","Isabella","Skyla","Zachary","Jude","Hailey","Emily","Kayla","Jessica","Carol","Alex","Noah","Sadie","Michael","Julia","Sarah","Ashley","Madison","Howard","Sheryl","Adriyel"]
+   const randomName = name[Math.floor(Math.random() * name.length)];
+
+  //Compliments
+   const compliments = ["Wow You're a SUPERSTAR ⭐!!!!", "Your Awesome ❤️!!!!","Thank you Math Wiz 🔢!!!","Thank you ❤️!!!!", " I appreaciate this❤️!!!!❤️"];
+   const randomcompliment = compliments[Math.floor(Math.random() * compliments.length)];
+  document.getElementById("questionTitle").innerHTML= `Customer 10 (Question 10)`
+
+  document.getElementById("question").innerHTML= `<b> ${randomName} </b> I have 32 pennies, 24 nickels, 42 dimes, 48 quarters, and 19 fifty-cents... . How much money do I have? ` 
+
+  var input = document.getElementById("answerInput").value;
+
+    console.log(input)
+    var correct = 27.22 ;
+
+  if(input == correct){
+     document.getElementById("display").innerHTML=` <b> ${randomName}:</b> ${randomcompliment} <br><button onclick="extremeLevel2End()"> Finish Day 2 </button>`
+
+  }
+    else {
+      document.getElementById("display").innerHTML=`Try Again`
+    }
+}
+// Extreme  DAY 2 (LEVEL 2)  CUSTOMER 10 (QUESTION 10) END
+
+function extremeLevel2End(){
+  document.getElementById("display").innerHTML=` <b> Boss :</b> Congratulations on Completing Day 2 <br> <a href="extremeLevelddd.html"><button> Move on to Extreme Day 3 </button> </a>`
+}
